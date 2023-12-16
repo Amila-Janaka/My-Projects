f6b4fb7a30b0d7b7bbe731222fc1e41636edda93
Amila Janaka <janakaamila28@gmail.com>
2023-12-16 15:28:59 +0530

    dn full hari

--- app/src/main/java/com/example/advanceapplication/DBHelper.java
+++ app/src/main/java/com/example/advanceapplication/DBHelper.java
@@ -1,6 +1,8 @@
 package com.example.advanceapplication;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -14,12 +16,74 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE students(reg_no TEXT PRIMARY KEY,name TEXT,bday TEXT,email TEXT)");
+        db.execSQL("CREATE TABLE students(regNo TEXT PRIMARY KEY,name TEXT,bDay TEXT,email TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         db.execSQL("DROP TABLE IF EXISTS students");
         onCreate(db);
+    }
+
+    public boolean insertData(String regParam, String nameParam, String bDayParam, String emailParam) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues conValues = new ContentValues();
+        conValues.put("regNo", regParam);
+        conValues.put("name", nameParam);
+        conValues.put("bDay", bDayParam);
+        conValues.put("email", emailParam);
+        long result = db.insert("students", null, conValues);
+        if (result == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public boolean updateData(String regParam, String nameParam, String bDayParam, String emailParam) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues conValues = new ContentValues();
+        if (nameParam != null) {
+            conValues.put("name", nameParam);
+        }
+        if (nameParam != null) {
+            conValues.put("bDay", bDayParam);
+        }
+        if (nameParam != null) {
+            conValues.put("email", emailParam);
+        }
+        Cursor cursor = db.rawQuery("SELECT * FROM students WHERE regNo = ?", new String[]{regParam});
+        if (cursor.getCount() > 0) {
+            cursor.close();
+            long result = db.update("students", conValues, "regNo = ?", new String[]{regParam});
+            if (result == -1) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+        cursor.close();
+        return false;
+    }
+
+    public boolean deleteData(String regParam) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM students WHERE regNo = ?", new String[]{regParam});
+        if (cursor.getCount() > 0) {
+            cursor.close();
+            long result = db.delete("students", "regNo = ?", new String[]{regParam});
+            if (result == -1) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+        cursor.close();
+        return false;
+    }
+
+    public Cursor getData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.rawQuery("SELECT * FROM students",null);
     }
 }
--- app/src/main/java/com/example/advanceapplication/MainActivity.java
+++ app/src/main/java/com/example/advanceapplication/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.advanceapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText layout_stdNum, layout_stdName, layout_stdBirthday, layout_stdEmail;
     Button insertBtn, updateBtn, deleteBtn, viewBtn;
+
+    String regNo, fullName, birthDay, email;
+
+    DBHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +33,109 @@ public class MainActivity extends AppCompatActivity {
         deleteBtn = findViewById(R.id.btn_delete);
         viewBtn = findViewById(R.id.btn_view);
 
+        db = new DBHelper(MainActivity.this);
+
+        insertBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (validateData(1)) {
+                    if (db.insertData(regNo, fullName, birthDay, email)) {
+                        Toast.makeText(MainActivity.this, "Successful !", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Unsuccessful !", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+        updateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (validateData(2)) {
+                    if (db.updateData(regNo, fullName, birthDay, email)) {
+                        Toast.makeText(MainActivity.this, "Successful !", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Unsuccessful !", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+        deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (validateData(3)) {
+                    if (db.deleteData(regNo)) {
+                        Toast.makeText(MainActivity.this, "Successful !", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Unsuccessful !", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+        viewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor cursor = db.getData();
+                if (cursor.getCount() > 0) {
+                    StringBuilder data = new StringBuilder();
+                    while (cursor.moveToNext()) {
+                        data.append("Registration Number : ").append(cursor.getString(0)).append("\n");
+                        data.append("Registration Name : ").append(cursor.getString(1)).append("\n");
+                        data.append("Registration Birthday : ").append(cursor.getString(2)).append("\n");
+                        data.append("Registration Email : ").append(cursor.getString(3)).append("\n\n");
+                    }
+                    AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                    alert.setTitle("Database Data");
+                    alert.setMessage(data.toString());
+                    alert.setCancelable(true);
+                    alert.show();
+                } else {
+                    Toast.makeText(MainActivity.this, "No Data in Database", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    //type == 1 (insert data)
+    //type == 2 (update data)
+    private boolean validateData(int type) {
+        if (layout_stdNum.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Enter your registration number !", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (type == 1) {
+            if (layout_stdName.getText().toString().isEmpty()) {
+                Toast.makeText(this, "Enter your full name !", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+            if (layout_stdBirthday.getText().toString().isEmpty()) {
+                Toast.makeText(this, "Enter your birthday !", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+            if (layout_stdEmail.getText().toString().isEmpty()) {
+                Toast.makeText(this, "Enter your email !", Toast.LENGTH_SHORT).show();
+
+            }
+            return true;
+        } else if (type == 2) {
+            boolean x = false;
+            if (!layout_stdName.getText().toString().isEmpty()) {
+                x = true;
+            }
+            if (!layout_stdBirthday.getText().toString().isEmpty()) {
+                x = true;
+            }
+            if (!layout_stdEmail.getText().toString().isEmpty()) {
+                x = true;
+            }
+            if (!x) {
+                Toast.makeText(this, "Fill one or more to continue !", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        }
+        regNo = layout_stdNum.getText().toString().isEmpty() ? null : layout_stdNum.getText().toString();
+        fullName = layout_stdName.getText().toString().isEmpty() ? null : layout_stdName.getText().toString();
+        birthDay = layout_stdBirthday.getText().toString().isEmpty() ? null : layout_stdBirthday.getText().toString();
+        email = layout_stdEmail.getText().toString().isEmpty() ? null : layout_stdEmail.getText().toString();
+        return true;
     }
 }
